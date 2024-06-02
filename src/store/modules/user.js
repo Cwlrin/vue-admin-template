ab13503c84ceacbcd5b9ad3db6dcdffd65ac3abe
桜小路七葉 <Nana7ha@outlook.com>
2024-06-02 22:27:38 +0800

    fix: 处理 token 失效的问题

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -35,6 +35,11 @@ const actions = {
   async getUserInfo(context) {
     const result = await getUserInfo()
     context.commit('setUserInfo', result)
+  },
+  // 退出登录的action
+  logout(context) {
+    context.commit('removeToken') // 删除token
+    context.commit('setUserInfo', {}) // 设置用户信息为空对象
   }
 }
 
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import store from '@/store'
 import { Message } from 'element-ui'
+import router from '@/router'
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // 基础地址
   timeout: 10000
@@ -8,7 +9,7 @@ const service = axios.create({
 // 成功1 失败2
 service.interceptors.request.use((config) => {
   // 注入token
-//  this.$store.getters
+  // this.$store.getters
   // store.getters.token => 请求头里面
   if (store.getters.token) {
     config.headers.Authorization = `Bearer ${store.getters.token}`
@@ -29,9 +30,17 @@ service.interceptors.response.use((response) => {
     Message({ type: 'error', message })
     return Promise.reject(new Error(message))
   }
-}, (error) => {
+}, async(error) => {
+  if (error.response.status === 401) {
+    Message({ type: 'warning', message: 'token超时了' })
+    // 说明token超时了
+    await store.dispatch('user/logout') // 调用action 退出登录
+    //  主动跳到登录页
+    router.push('/login') // 跳转到登录页
+    return Promise.reject(error)
+  }
   // error.message
-  Message({ type: 'error', message: error.message })
+  Message.error(error.message)
   return Promise.reject(error)
 })
 export default service
