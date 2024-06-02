401131b103fb57f11ee1c77b189077699bd6fc04
桜小路七葉 <Nana7ha@outlook.com>
2024-06-02 22:18:34 +0800

    fix: 处理头像为空的场景

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -8,11 +8,12 @@
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
           <!-- 头像 -->
-          <img :src="avatar" class="user-avatar">
+          <img v-if="avatar" :src="avatar" class="user-avatar">
+          <span v-else class="username">{{ name?.charAt(0) }}</span>
           <!-- 用户名称 -->
           <span class="name">{{ name }}</span>
           <!-- 图标 -->
-          <i class="el-icon-caret-bottom" />
+          <i class="el-icon-setting" />
         </div>
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
           <router-link to="/">
@@ -130,6 +131,17 @@ export default {
           //  用户名称距离右侧距离
           margin-right: 10px;
           font-size: 16px;
+        }
+
+        .username {
+          width: 30px;
+          height: 30px;
+          text-align: center;
+          line-height: 30px;
+          border-radius: 50%;
+          background: #04c9be;
+          color: #fff;
+          margin-right: 4px;
         }
 
         .el-icon-setting {
