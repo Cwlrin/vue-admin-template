5b390ee8af7b3c8cb06214b09ae25d7d0b15d650
桜小路七葉 <Nana7ha@outlook.com>
2024-06-04 18:08:44 +0800

    feat: excel 上传下载

--- src/api/employee.js
+++ src/api/employee.js
@@ -9,3 +9,35 @@ export function getEmployeeList(params) {
     params // 地址参数 查询参数
   })
 }
+
+/**
+ * 导出员工的excel
+ */
+export function exportEmployee() {
+  return request({
+    url: '/sys/user/export',
+    // 改变接收数据的类型
+    responseType: 'blob' // 使用blob接收二进制文件流
+  })
+}
+
+/**
+ * 下载员工导入模版
+ */
+export function getExportTemplate() {
+  return request({
+    url: '/sys/user/import/template',
+    responseType: 'blob' // 二进制文件流
+  })
+}
+
+/**
+ * 上传用户的excel
+ */
+export function uploadExcel(data) {
+  return request({
+    url: '/sys/user/import',
+    method: 'post',
+    data // form-data类型 因为要上传文件类型
+  })
+}
