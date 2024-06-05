8f99aa21ce1f1052e65a0aafa03f2aabd391aa44
桜小路七葉 <Nana7ha@outlook.com>
2024-06-05 22:20:36 +0800

    feat: 根据权限显示左侧菜单

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,8 +1,11 @@
 import { getToken, setToken, removeToken } from '@/utils/auth'
 import { login, getUserInfo } from '@/api/user'
+import { constantRoutes } from '@/router'
+
 const state = {
   token: getToken(), // 从缓存中读取初始值
-  userInfo: {} // 存储用户基本资料状态
+  userInfo: {}, // 存储用户基本资料状态
+  routes: constantRoutes // 静态路由的数组
 }
 
 const mutations = {
@@ -10,14 +13,14 @@ const mutations = {
     state.token = token
     // 同步到缓存
     setToken(token)
-  },
-  removeToken(state) {
+  }, removeToken(state) {
     // 删除Vuex的token
     state.token = null
     removeToken()
-  },
-  setUserInfo(state, userInfo) {
+  }, setUserInfo(state, userInfo) {
     state.userInfo = userInfo
+  }, setRoutes(state, newRoutes) {
+    state.routes = [...constantRoutes, ...newRoutes] // 静态路由 + 动态路由
   }
 }
 
@@ -29,14 +32,12 @@ const actions = {
     const token = await login(data)
     // 返回一个token 123456
     context.commit('setToken', token)
-  },
-  // 获取用户的基本资料
+  }, // 获取用户的基本资料
   async getUserInfo(context) {
     const result = await getUserInfo()
     context.commit('setUserInfo', result)
     return result // 返回数据
-  },
-  // 退出登录的action
+  }, // 退出登录的action
   logout(context) {
     context.commit('removeToken') // 删除token
     context.commit('setUserInfo', {}) // 设置用户信息为空对象
@@ -45,7 +46,5 @@ const actions = {
 
 export default {
   namespaced: true, // 开启命名空间
-  state,
-  mutations,
-  actions
+  state, mutations, actions
 }
